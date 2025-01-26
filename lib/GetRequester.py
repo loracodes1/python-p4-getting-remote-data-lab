c7df644fcb057f943ce75ac33dfae93dc0436099
florence <loracodes3@gmail.com>
2025-01-27 00:16:29 +0300

    commits

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,15 +1,18 @@
+# lib/GetRequester.py
+
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
+        # Send GET request to the URL
         response = requests.get(self.url)
-        return response.text
+        # Decode the response content to a string
+        return response.content.decode("utf-8")
 
     def load_json(self):
+        # Parse the JSON from the decoded response body
         response_body = self.get_response_body()
         return json.loads(response_body)
